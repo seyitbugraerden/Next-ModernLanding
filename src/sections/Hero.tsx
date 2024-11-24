74c2cab88f96b871dc9cea57d873e01e6a93d05e
seyitbugraerden <seyitbugraerden@gmail.com>
2024-11-24 22:43:33 +0300

    side images

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,9 +1,24 @@
 import Button from "@/components/Button";
+import designExample1Image from "@/assets/images/design-example-1.png";
+import designExample2Image from "@/assets/images/design-example-2.png";
+import Image from "next/image";
 
 export default function Hero() {
     return (
-        <section className="py-24">
-            <div className="container">
+        <section className="py-24 overflow-x-clip">
+            <div className="container relative">
+                <div className="absolute -left-32 top-16">
+                    <Image
+                        src={designExample1Image}
+                        alt="Design Example 1 Image"
+                    />
+                </div>
+                <div className="absolute -right-64 top-16">
+                    <Image
+                        src={designExample2Image}
+                        alt="Design Example 2 Image"
+                    />
+                </div>
                 <div className="flex justify-center">
                     <div className="flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-970 font-semibold">
                         ✨ $7.5M seed round raised
