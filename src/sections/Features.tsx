124bcf149ef6724b7a91a60d745637c05a9dd37c
seyitbugraerden <seyitbugraerden@gmail.com>
2024-11-28 22:02:36 +0300

    Colerfull text section

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,6 +1,10 @@
 import Tag from "@/components/Tag";
-import { div } from "framer-motion/client";
-
+import FeatureCard from "@/components/FeatureCard";
+import avatar1 from "@/assets/images/avatar-ashwin-santiago.jpg";
+import avatar2 from "@/assets/images/avatar-lula-meyers.jpg";
+import avatar3 from "@/assets/images/avatar-florence-shaw.jpg";
+import Image from "next/image";
+import Avatar from "@/components/Avatar";
 const features = [
     "Asset Library",
     "Code Preview",
@@ -13,43 +17,109 @@ const features = [
 
 export default function Features() {
     return (
-        <section>
+        <section className="py-24">
             <div className="container">
-                <Tag>Features</Tag>
-                <h2>
-                    Where power meets <span>simplicity</span>
+                <div className="flex justify-center">
+                    <Tag>Features</Tag>
+                </div>
+                <h2 className="text-6xl font-medium text-center mt-6">
+                    Where power meets{" "}
+                    <span className="text-lime-400">simplicity</span>
                 </h2>
-                <div>
-                    <div>
-                        <div></div>
-                        <div>
-                            <h3>Real-time Collaboration</h3>
-                            <p>
-                                Work together seamless with conflict-free team
-                                editing
+                <div className="mt-12 grid grid-cols-1 gap-8">
+                    <FeatureCard
+                        title="Real-time Collaboration"
+                        description="Work together seamless with conflict-free team
+                                editing"
+                    >
+                        <div className="aspect-square flex items-center justify-center">
+                            <Avatar className="z-40">
+                                <Image
+                                    src={avatar1}
+                                    alt="Avatar1"
+                                    className="rounded-full"
+                                />
+                            </Avatar>
+                            <Avatar className="-ml-6 border-indigo-500 z-30">
+                                <Image
+                                    src={avatar2}
+                                    alt="Avatar2"
+                                    className="rounded-full"
+                                />
+                            </Avatar>
+                            <Avatar className="-ml-6 border-amber-500 z-20">
+                                <Image
+                                    src={avatar3}
+                                    alt="Avatar3"
+                                    className="rounded-full"
+                                />
+                            </Avatar>
+                            <Avatar className="-ml-6 border-transparent rounded-full">
+                                <div className="size-full bg-neutral-700 inline-flex justify-center items-center gap-1">
+                                    {Array.from({ length: 3 }).map((_, i) => (
+                                        <span
+                                            key={i}
+                                            className="size-1.5 rounded-full bg-white inline-flex"
+                                        ></span>
+                                    ))}
+                                </div>
+                            </Avatar>
+                        </div>
+                    </FeatureCard>
+                    <FeatureCard
+                        title="Interactive Prototyping"
+                        description="Engage your clients with prototypes that react
+                                to user actions"
+                    >
+                        <div className="aspect-video flex items-center justify-center">
+                            <p className="text-4xl font-extrabold text-white/20 text-center ">
+                                We&apos;ve achived{" "}
+                                <span className="bg-gradient-to-r from-purple-400 to-pink-400 bg-clip-text text-transparent">
+                                    incredible
+                                </span>{" "}
+                                growth this year
                             </p>
                         </div>
-                    </div>
-                    <div>
-                        <div></div>
-                        <div>
-                            <h3>Interactive Prototyping</h3>
-                            <p>
-                                Engage your clients with prototypes that react
-                                to user actions
-                            </p>
+                    </FeatureCard>
+                    <FeatureCard
+                        title="Keyboard Quick Actions"
+                        description="Powerful comments to help you create designs
+                                more quickly"
+                    >
+                        <div className="aspect-square flex items-center justify-center">
+                            <Avatar className="z-40">
+                                <Image
+                                    src={avatar1}
+                                    alt="Avatar1"
+                                    className="rounded-full"
+                                />
+                            </Avatar>
+                            <Avatar className="-ml-6 border-indigo-500 z-30">
+                                <Image
+                                    src={avatar2}
+                                    alt="Avatar2"
+                                    className="rounded-full"
+                                />
+                            </Avatar>
+                            <Avatar className="-ml-6 border-amber-500 z-20">
+                                <Image
+                                    src={avatar3}
+                                    alt="Avatar3"
+                                    className="rounded-full"
+                                />
+                            </Avatar>
+                            <Avatar className="-ml-6 border-transparent rounded-full inline-flex items-center justify-center gap-1">
+                                <div className="size-full bg-neutral-700">
+                                    {Array.from({ length: 3 }).map((_, i) => (
+                                        <span
+                                            key={i}
+                                            className="size-1.5 rounded-full bg-white inline-flex"
+                                        ></span>
+                                    ))}
+                                </div>
+                            </Avatar>
                         </div>
-                    </div>
-                    <div>
-                        <div></div>
-                        <div>
-                            <h3>Keyboard Quick Actions</h3>
-                            <p>
-                                Powerful comments to help you create designs
-                                more quickly
-                            </p>
-                        </div>
-                    </div>
+                    </FeatureCard>
                 </div>
                 <div>
                     {features.map((x: string, idx: number) => {
