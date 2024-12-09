92f3796bf8aca80854731a396e9ec58a431a98fc
seyitbugraerden <seyitbugraerden@gmail.com>
2024-12-10 00:00:03 +0300

    motion effects

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -5,15 +5,53 @@ import designExample2Image from "@/assets/images/design-example-2.png";
 import Image from "next/image";
 import Pointer from "@/components/Pointer";
 import { motion, useAnimate } from "framer-motion";
+import { useEffect } from "react";
 
 export default function Hero() {
     const [leftDesignScope, leftDesignAnimate] = useAnimate();
     const [leftPointerScope, leftPointerAnimate] = useAnimate();
+    const [rightDesignScope, rightDesignAnimate] = useAnimate();
+    const [rightPointerScope, rightPointerAnimate] = useAnimate();
+
+    useEffect(() => {
+        leftDesignAnimate([
+            [leftDesignScope.current, { opacity: 1 }, { duration: 0.5 }],
+            [leftDesignScope.current, { y: 0, x: 0 }, { duration: 0.5 }],
+        ]);
+        leftPointerAnimate([
+            [leftPointerScope.current, { opacity: 1 }, { duration: 0.5 }],
+            [leftPointerScope.current, { y: 0, x: -100 }, { duration: 0.5 }],
+            [
+                leftPointerScope.current,
+                { x: 0, y: [0, 16, 0] },
+                { duration: 0.5, ease: "easeInOut" },
+            ],
+        ]);
+        rightDesignAnimate([
+            [
+                rightDesignScope.current,
+                { opacity: 1 },
+                { duration: 0.5, delay: 1.5 },
+            ],
+            [rightDesignScope.current, { x: 0, y: 0 }, { duration: 0.5 }],
+        ]);
+        rightPointerAnimate([
+            [
+                rightPointerScope.current,
+                { opacity: 1 },
+                { duration: 0.5, delay: 1.5 },
+            ],
+            [rightPointerScope.current, { x: 175, y: 0 }, { duration: 0.5 }],
+            [rightPointerScope.current, { x: 0, y: [0,20,0] }, { duration: 0.5 }],
+        ]);
+    }, []);
+
     return (
         <section className="py-24 overflow-x-clip">
             <div className="container relative">
                 <motion.div
                     ref={leftDesignScope}
+                    initial={{ opacity: 0, y: 100, x: -100 }}
                     className="absolute -left-32 top-16"
                 >
                     <Image
@@ -24,20 +62,29 @@ export default function Hero() {
                 </motion.div>
                 <motion.div
                     ref={leftPointerScope}
+                    initial={{ opacity: 0, y: 100, x: -200 }}
                     className="absolute left-56 top-96 hidden lg:block"
                 >
                     <Pointer name="Andrea" />
                 </motion.div>
-                <div className="absolute -right-64 top-16">
+                <motion.div
+                    ref={rightDesignScope}
+                    initial={{ opacity: 0, x: 100, y: 100 }}
+                    className="absolute -right-64 top-16"
+                >
                     <Image
                         className="hidden lg:block"
                         src={designExample2Image}
                         alt="Design Example 2 Image"
                     />
-                </div>
-                <div className="absolute right-56 top-0 hidden lg:block">
+                </motion.div>
+                <motion.div
+                    ref={rightPointerScope}
+                    initial={{ opacity: 0, x: 250, y: 100 }}
+                    className="absolute right-56 top-0 hidden lg:block"
+                >
                     <Pointer name="Bryan" color="red" />
-                </div>
+                </motion.div>
                 <div className="flex justify-center">
                     <div className="flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-970 font-semibold">
                         ✨ $7.5M seed round raised
