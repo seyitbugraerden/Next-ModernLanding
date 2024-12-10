0f877753bbaf9b7cc60fe123b4b9b6a9005947c9
seyitbugraerden <seyitbugraerden@gmail.com>
2024-12-11 00:39:46 +0300

    Call to Action

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,8 +1,20 @@
+"use client";
+import { motion } from "framer-motion";
 export default function CallToAction() {
     return (
         <section className="py-24">
             <div className="overflow-x-clip p-4 flex">
-                <div className="flex flex-none gap-16 text-7xl md:text-8xl font-medium">
+                <motion.div
+                    animate={{
+                        x: "-50%",
+                    }}
+                    transition={{
+                        duration: 50,
+                        ease: "linear",
+                        repeat: Infinity,
+                    }}
+                    className="flex flex-none gap-16 pr-16 text-7xl md:text-8xl font-medium"
+                >
                     {Array.from({ length: 10 }).map((_, i) => (
                         <div key={i} className="flex items-center gap-16">
                             <span className="text-lime-400 text-7xl">
@@ -11,7 +23,7 @@ export default function CallToAction() {
                             <span>Try it for free</span>
                         </div>
                     ))}
-                </div>
+                </motion.div>
             </div>
         </section>
     );
